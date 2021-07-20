853dbfcccdd6b5aa85d486c42606275f5c58c686
Anton Mishanin <antonmishanin@mail.ru>
2021-07-20 20:54:50 +0700

    feat: add in-app updater.

--- app/src/main/java/com/paint/bindingtoolbarfragment/MainActivity.kt
+++ app/src/main/java/com/paint/bindingtoolbarfragment/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.paint.bindingtoolbarfragment
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.paint.bindingtoolbarfragment.databinding.ActivityMainBinding
+
+//Docs: https://developer.android.com/guide/playcore/in-app-updates/kotlin-java
+
+//Auto update
+//implementation 'com.google.android.play:core:1.10.0'
+
+const val UPDATE_REQUEST_CODE = 8
+private const val TAG = "UpdateManager"
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +32,24 @@ class MainActivity : AppCompatActivity() {
         binding.layout.buttonInLayout.setOnClickListener {
             val intent = Intent(this, WithFragmentActivity::class.java)
             startActivity(intent)
+        }
+
+        checkForUpdate(activity = this)
+    }
+
+    private fun checkForUpdate(activity: Activity) {
+        Log.d(TAG, "checkForUpdate")
+        val appUpdateManager = AppUpdateManagerFactory.create(activity.applicationContext)
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    activity,
+                    UPDATE_REQUEST_CODE)
+            }
         }
     }
 }
